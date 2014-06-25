4b06e17583b6feffa1d1d3ed7f0995d327a4fcc6
zoewangcn <zoe.zywang.cn@gmail.com>
2014-06-25 10:53:00 +0800

    none

--- DMHIN.cpp
+++ DMHIN.cpp
@@ -5,9 +5,18 @@
 #include "Vertex.h"
 #include "Link.h"
 #include "HIN.h"
+#include "DBConnection.h"
 HIN network;
 const float lamuda = 0.2;
 const float a = 0.1;
+void readData()
+{
+
+}
+void rankClass(HIN hin, float lamuda, float a)
+{
+
+}
 int _tmain(int argc, _TCHAR* argv[])
 {
 	return 0;
